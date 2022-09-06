558d637a6b6a446e863333aa5ea23a67236aa534
StefanGarofalo <garofalo.s@outlook.com>
2022-09-06 09:15:32 +0200

    Pages created Main App component created Router linked to main.js root file

--- src/router.js
+++ src/router.js
@@ -1,16 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import CoachDetails from './pages/coaches/CoachDetails.vue'
+import CoachesList from './pages/coaches/CoachesList.vue'
+import CoachRegistration from './pages/coaches/CoachRegistration.vue'
+//----------------------
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestsReceived from './pages/requests/RequestsReceived.vue'
+//----------------------
+import NotFound from './pages/NotFound.vue'
+//----------------------
+
 const router = createRouter({
   createWebHistory,
   routes: [
     { path: '/', redirect: '/coaches'},
-    { path: '/coaches', component: null},
-    { path: '/coaches/:id', component: null, children: 
-      [{ path: '/contact', component: null }]
+    { path: '/coaches', component: CoachesList},
+    { path: '/coaches/:id', component: CoachDetails, children: 
+      [{ path: '/contact', component: ContactCoach }]
     },
-    { path: '/register', component: null},
-    { path: '/requests', component: null},
-    { path: '/:notFound(.*)', component: null}
+    { path: '/register', component: CoachRegistration},
+    { path: '/requests', component: RequestsReceived},
+    { path: '/:notFound(.*)', component: NotFound}
   ]
 })
 
