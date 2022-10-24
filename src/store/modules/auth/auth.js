c3a48c1ab12f346b8c0bfbadd5522a71005299f6
StefanGarofalo <garofalo.s@outlook.com>
2022-10-24 07:21:39 +0200

    refactor: merge sinup-login into auth action

--- src/store/modules/auth/auth.js
+++ src/store/modules/auth/auth.js
@@ -21,51 +21,67 @@ const authModule = {
         expire: null
       })
     },
+    
     async login(context, payload){
-      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDKijAAMOqyLPPVntw6qq_818l06yulWsA ',{
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      })
-
-      const data = await response.json()
-
-      if(!response.ok){
-        throw new Error(data.message || 'Failed to auth')
-      }
-
-      context.commit('setUser', {
-        token: data.idToken,
-        userId: data.localId,
-        expire: data.expiresIn
-      })
+      return context.dispatch('auth', { ...payload, mode: 'login' })
     },
 
     async signup(context, payload){
-      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDKijAAMOqyLPPVntw6qq_818l06yulWsA',{
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
+      return context.dispatch('auth', { ...payload, mode: 'login' })
+    },
+
+    async auth(context, payload){
+      const mode = payload.mode
+      const url = mode === 'login' ? 
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDKijAAMOqyLPPVntw6qq_818l06yulWsA' 
+        :
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDKijAAMOqyLPPVntw6qq_818l06yulWsA'
+      if(mode === 'login'){
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({
+            email: payload.email,
+            password: payload.password,
+            returnSecureToken: true
+          })
         })
-      })
-      
-      const data = await response.json()
 
-      if(!response.ok){
-        throw new Error(data.message || 'Failed to auth')
+        const data = await response.json()
+
+        if(!response.ok){
+          throw new Error(data.message || 'Failed to auth')
+        }
+
+        context.commit('setUser', {
+          token: data.idToken,
+          userId: data.localId,
+          expire: data.expiresIn
+        })
       }
+      if(mode === 'signup'){
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({
+            email: payload.email,
+            password: payload.password,
+            returnSecureToken: true
+          })
+        })
+        
+        const data = await response.json()
 
-      context.commit('setUser', {
-        token: data.idToken,
-        userId: data.localId,
-        expire: data.expiresIn
-      })
+        if(!response.ok){
+          throw new Error(data.message || 'Failed to auth')
+        }
+
+        context.commit('setUser', {
+          token: data.idToken,
+          userId: data.localId,
+          expire: data.expiresIn
+        })
+      }
     }
+      
   },
   getters: {
     userId(state) {
